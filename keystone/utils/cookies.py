504e3349f7fb311bafa8855c016fd63273908530
Sheldon Tower <towersheldon@gmail.com>
2026-02-22 14:25:57 -0330

    Changed cookies to same site = none for use with github pages

--- keystone/utils/cookies.py
+++ keystone/utils/cookies.py
@@ -2,14 +2,14 @@ from django.conf import settings
 
 def set_auth_cookie(response, name, value, max_age=None):
     """
-    Set a cookie with HttpOnly and Secure flags.
+    Set a cookie with HttpOnly and Secure flags for cross-site usage.
     """
     response.set_cookie(
         name,
         value,
         max_age=max_age,
         httponly=True,
-        secure=not settings.DEBUG,
-        samesite='Lax' if settings.DEBUG else 'Strict',
+        secure=True,  # Required for SameSite=None
+        samesite='None',  # Allow cross-site cookie usage
         path='/'
     )
